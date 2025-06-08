521fbcc6c3bc1c0f30931081d9158c576fa45945
chad morrison <134284764+Chedder78@users.noreply.github.com>
2025-06-07 23:45:32 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,5 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
 import ARManager from './components/ARManager'
-import './styles.css'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <ARManager />
-  </StrictMode>
-)
+export default function App() {
+  return <ARManager />
+}
