4f33eeaada7599d16037831f87c1afc84ecdcff9
chad morrison <134284764+Chedder78@users.noreply.github.com>
2025-06-07 23:55:35 -0700

    Create LoadingScreen.jsx

---
+++ (src/components/UI/LoadingScreen.jsx
@@ -0,0 +1,8 @@
+export default function LoadingScreen() {
+  return (
+    <div className="loading-overlay">
+      <div className="loading-spinner"></div>
+      <p>Loading magical underwater world...</p>
+    </div>
+  )
+}
